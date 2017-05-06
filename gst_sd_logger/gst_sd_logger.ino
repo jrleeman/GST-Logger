d4e7476982dd1b70a0b8a6cf7bf735a3c4275965
J.R. Leeman <kd5wxb@gmail.com>
2017-05-06 08:29:44 -0600

    Fix SD pin assignment and tune

--- gst_sd_logger/gst_sd_logger.ino
+++ gst_sd_logger/gst_sd_logger.ino
@@ -12,20 +12,20 @@
  * https://github.com/adafruit/Adafruit-MLX90614-Library
  * 
 */
-#include <SoftwareSerial.h>
+//#include <SoftwareSerial.h>
+#include <WildFire.h>
 #include <SPI.h>
 #include <SD.h>
 #include <Wire.h>
-
 #include <Adafruit_MLX90614.h>
 #include <SparkFunISL29125.h>
 #include <TinyGPS++.h>
 
-// SD Pin connections
-const int chipSelect = 4;
+// Variables and defines
+#define chipSelect 16
 
 // GPS Pin connections
-static const int RXPin = 4, TXPin = 3;
+//static const int RXPin = 4, TXPin = 3;
 static const uint32_t GPSBaud = 4800;
 
 // Variable Declarations
@@ -39,13 +39,16 @@ Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 SFE_ISL29125 RGB_sensor;
 TinyGPSPlus gps;
 File logfile;
-
+WildFire wildfire;
 // The serial connection to the GPS device
-SoftwareSerial ss(RXPin, TXPin);
-
+//SoftwareSerial ss(RXPin, TXPin);
+char filename[16]; // make it long enough to hold your longest file name, plus a null terminator
 void setup() {
-  Serial.begin(9600);
-  ss.begin(GPSBaud);
+  wildfire.begin();
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  Serial1.begin(GPSBaud);
+  
   // Setup SD Card
   Serial.print("Initializing SD card...");
 
@@ -61,25 +64,27 @@ void setup() {
   mlx.begin(); 
   
   // Setup Color Sensor
+  
   if (RGB_sensor.init())
   {
     Serial.println("Sensor Initialization Successful\n\r");
   }
-
-  char filename[16]; // make it long enough to hold your longest file name, plus a null terminator
+  
+  
   int n = 0;
   snprintf(filename, sizeof(filename), "data%03d.txt", n); // includes a three-digit sequence number in the file name
   while(SD.exists(filename)) {
     n++;
     snprintf(filename, sizeof(filename), "data%03d.txt", n);
   }
-  logfile = SD.open(filename,FILE_READ);
+  Serial.print("Filename: ");
+  Serial.println(filename);
 
 }
 
 void loop() {
-  while (ss.available() > 0){
-    if (gps.encode(ss.read())){
+  while (Serial1.available() > 0){
+    if (gps.encode(Serial1.read())){
       read_IR_sensor();
       read_color_sensor();
       log_data();
@@ -89,51 +94,62 @@ void loop() {
 }
 
 void log_data() {
-
+  digitalWrite(LED_BUILTIN, HIGH);
+  logfile = SD.open(filename,FILE_WRITE);
+  
   // Format and write out the time stamp
   char sz[32];
   sprintf(sz, "%04d-%02d-%02dT%02d:%02d:%02d.%02d,",
   gps.date.year(), gps.date.month(), gps.date.day(), gps.time.hour(),
   gps.time.minute(), gps.time.second(), gps.time.centisecond());
-  logfile.write(sz);
 
-  // Log latitude
-  log_float(gps.location.lat(), -999.99, 10, 6);
-  logfile.write(",");
-
-  // Log longitude
-  log_float(gps.location.lng(), -999.99, 10, 6);
-  logfile.write(",");
-
-  // Log altitude
-  log_float(gps.altitude.meters(), -999.99, 10, 6);
-  logfile.write(",");
-
-  // Log speed
-  log_float(gps.speed.kmph(), -999.99, 10, 6);
-  logfile.write(",");
-
-  // Log heading
-  log_float(gps.course.deg(), -999.99, 10, 6);
-  logfile.write(",");
-
-  // Log number of satellites
-  log_float(gps.satellites.value(), -999.99, 10, 6);
-  logfile.write(",");
+  if (logfile) {
+    
+    logfile.write(sz);
+    Serial.print("Writing data...");
+    // Log latitude
+    log_float(gps.location.lat(), -999.99, 10, 6);
+    logfile.write(",");
   
-  // Log ambient temperature
-  log_float(IR_ambient_temperature, -999.99, 10, 6);
-  logfile.write(",");
-
-  // Log object temperature
-  log_float(IR_object_temperature, -999.99, 10, 6);
-  logfile.write(",");
-
-  // Log RGB values
-  char szz[32];
-  sprintf(sz, "%d,%d,%d,",
-  color_red, color_green, color_blue);
-  logfile.write(szz);
+    // Log longitude
+    log_float(gps.location.lng(), -999.99, 10, 6);
+    logfile.write(",");
+  
+    // Log altitude
+    log_float(gps.altitude.meters(), -999.99, 10, 6);
+    logfile.write(",");
+  
+    // Log speed
+    log_float(gps.speed.kmph(), -999.99, 10, 6);
+    logfile.write(",");
+  
+    // Log heading
+    log_float(gps.course.deg(), -999.99, 10, 6);
+    logfile.write(",");
+  
+    // Log number of satellites
+    log_float(gps.satellites.value(), -999.99, 10, 6);
+    logfile.write(",");
+    
+    // Log ambient temperature
+    log_float(IR_ambient_temperature, -999.99, 10, 6);
+    logfile.write(",");
+  
+    // Log object temperature
+    log_float(IR_object_temperature, -999.99, 10, 6);
+    logfile.write(",");
+  
+    // Log RGB values
+    
+    char szz[32];
+    sprintf(szz, "%d,%d,%d",
+    color_red, color_green, color_blue);
+    logfile.write(szz);
+    logfile.println("");
+    Serial.println("Done");
+  }
+  logfile.close();
+  digitalWrite(LED_BUILTIN, LOW);
   
   
 }
